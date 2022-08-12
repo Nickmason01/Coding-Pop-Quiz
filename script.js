ecfdeefed96c6c01b01d71c7053bde28d723d337
Nick Mason <nickmason372@yahoo.com>
2022-08-11 22:13:38 -0400

    Commit

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-var startButton =document.querySelector(".start-button");
-var timerEl =document.querySelector("#timer-text");
+var startButton = document.querySelector(".start-button");
+var timerEl = document.querySelector("#timer-text");
 var highScore = document.querySelector(".score-btn");
 var questionText = document.getElementById("questions-text");
 var answerOptions = document.querySelector(".answer-options");
+var scoresBtn = document.querySelector(".scores-btn");
+
 
 var timerCount;
 var timer;
-//var question = []
-var indexQuestion = 0; 
+let indexQuestion = 0;
 var score = 0;
 var possibleQuestions = [
-    
+
     {
-        question : "Commonly used data types DO NOT include",
-        options : [ 
+        question: "Commonly used data types DO NOT include",
+        options: [
             "Boolean",
             "numbers",
             "strings",
@@ -23,14 +24,14 @@ var possibleQuestions = [
 
         ],
         answer: "alerts"
-           
-        
-        
+
+
+
     },
 
     {
         question: "The condition of an If/Else statement is enclosed within ",
-        options : [
+        options: [
             "commas",
             "quotes",
             "parentheses",
@@ -38,43 +39,43 @@ var possibleQuestions = [
 
         ],
         answer: "parentheses"
-           
-        
-       
+
+
+
     },
 
     {
-        question : "Arrays in Javascript can be used to store ",
-        options : [
+        question: "Arrays in Javascript can be used to store ",
+        options: [
             "numbers and strings",
             "other arrays",
             "booleans",
-            "all the above",
-        ], 
+            "all of the above",
+        ],
         answer: "all of the above"
-            
-          
-        
-       
+
+
+
+
     },
 
     {
-        question : "String values must be enclosed by _____ when assigned as values",
-        options : [
+        question: "String values must be enclosed by _____ when assigned as values",
+        options: [
             "parentheses",
             "quotes",
             "square brackets",
             "none of the above",
 
         ],
-        answer:"quotes"
-           
-        
+        answer: "quotes"
+
+
     },
 
     {
-        question : "A very useful tool during development and debugging for printing content to the debugger is", 
-        options : [
+        question: "A very useful tool during development and debugging for printing content to the debugger is",
+        options: [
             "HTML",
             "for loops",
             "console.log",
@@ -82,9 +83,9 @@ var possibleQuestions = [
 
         ],
         answer: "console.log"
-           
-        
-     
+
+
+
     },
 
 ]
@@ -94,69 +95,87 @@ var possibleQuestions = [
 function startQuiz() {
     timerCount = 60;
     startButton.setAttribute("style", "display: none");
-    renderQuestion ();
-    startTimer ();
-    
+    renderQuestion();
+    startTimer();
+
 }
 
 function startTimer() {
-    timer =setInterval(function () {
+    timer = setInterval(function () {
         timerCount--;
         timerEl.textContent = timerCount + " seconds remaining";
         if (timerCount === 0) {
             clearInterval(timer);
-            gameOver ();
+            gameOver();
         }
 
 
     }, 1000);
-    
+
 }
 
 
-function renderQuestion () {
- var currentQuestion = possibleQuestions[indexQuestion];
- questionText.textContent = currentQuestion.question;
-  for (let index = 0; index < currentQuestion.options.length; index++) {
- var button = document.createElement("button");
- button.textContent = currentQuestion.options[index];
- answerOptions.appendChild(button);
-}
+function renderQuestion() {
+    //  button.setAttribute("onclick", "userAnswer()");
+    var currentQuestion = possibleQuestions[indexQuestion];
+    questionText.textContent = currentQuestion.question;
+    answerOptions.innerHTML = "";
+    for (let index = 0; index < currentQuestion.options.length; index++) {
+        var button = document.createElement("button");
+        button.textContent = currentQuestion.options[index];
+        button.addEventListener("click", function () { userAnswer(currentQuestion.options[index]) });
+        answerOptions.appendChild(button);
+    }
 
 
 }
 console.log(renderQuestion);
 
-function userAnswer () {
-    var currentAnswers=possibleQuestions[indexQuestion];
-    answerOptions.textContent=currentAnswers.options;
-    for(let index =0; index < currentAnswers.options.length; index++){
-        if(timerCount ===0){
-            return;
-        }
-    
-    
-        if(answerOptions === answer){
-            score++;
-        }
+function userAnswer(answer) {
+    var currentAnswer = possibleQuestions[indexQuestion].answer;
+    if (answer === currentAnswer){
+        score++; 
+        console.log(score)
+       // var tag = document.createElement("p");
+      // var text= document.createTextNode("correct!");
+       //tag.appendChild(text);
+       // var element = document.getElementById("correct-not-correct");
+//element.appendChild(text);
+        
 
+    }else{
+        timerCount -=10;
     }
+    //if(timerCount===0 || indexQuestion === 6) {
+       // clearInterval(renderQuestion);
+       // gameOver();
+    //}
+        //console.log("correct")
+        //console.log("not correct")
+        indexQuestion++
+        if(indexQuestion >= possibleQuestions.length) {
+            questionText.remove()
+            answerOptions.remove()
+            gameOver();
+        }else{
+            renderQuestion();
 
-   
+        }
+        
+}
 
+function gameOver() {
+timerEl.remove()
+    console.log("Gameover")
 
 }
-console.log (score)
-function nextQuestion () {
-    
-}
 
-function gameOver () {
+function highScores () {
 
 }
-
 
 
 
 startButton.addEventListener("click", startQuiz);
-answerOptions.addEventListener("click", userAnswer);
+scoresBtn.addEventListener("click", highScores);
+
